28a7d09f9b56b311ffed8a07725cd55a10220daa
Carl <carlpmac+jbgit@gmail.com>
2015-10-22 16:37:09 -0400

    updated mkiso.sh

--- mkiso.sh
+++ mkiso.sh
@@ -1,4 +1,4 @@
-mkisofs -o /home/carl/Iso/biosiso/freedos_biosupdate.iso -q -l -N \
+mkisofs -o ../biosupdate.iso -q -l -N \
    -boot-info-table -iso-level 4 -no-emul-boot \
    -b isolinux/isolinux.bin \
    -publisher "https://github.com/carlpmac/biosupdateiso/" \
